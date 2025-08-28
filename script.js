fb74a5da6cf0303adf44b9a86710aea519a504f0
Demo17-png <demolaorolu@yahoo.com>
2025-08-28 15:22:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Get the button element
+const scrollToTopBtn = document.getElementById("scroll-to-top");
+
+// Function to handle the scroll event
+window.onscroll = function() {
+    // Show the button if the user has scrolled more than 200px
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.classList.add("show");
+    } else {
+        scrollToTopBtn.classList.remove("show");
+    }
+};
+
+// Function to handle the click event
+scrollToTopBtn.onclick = function() {
+    // Smoothly scroll to the top of the document
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+    const sliderTrack = document.getElementById("sliderTrack");
+
+    // Toggle pause/play on click or tap
+    sliderTrack.addEventListener("click", () => {
+      sliderTrack.classList.toggle("paused");
+    });
+
+};
